ea7e125b88cb16af95b2228af3bb93a47b26ef2b
Xiaotian(Tiana) Lyu <lyu.xiao@northeastern.edu>
2024-09-22 15:47:59 -0700

    bbg

--- leetcode/src/company/bloomberg/goodluck/Q1347.java
+++ leetcode/src/company/bloomberg/goodluck/Q1347.java
@@ -1,6 +1,10 @@
 package company.bloomberg.goodluck;
 
 public class Q1347 {
+    // 记录两个字符串不相同的字符出现的次数
+    // 因为只要替换一个字符，就可以解决两个不同的字符
+    // 比如说，"a"和"b"虽然相差一个a和一个b，但是只需要替换一次就可以变成一样
+    // 所以最后的结果要除以2
     public int minSteps(String s, String t) {
         int[] count = new int[26];
         for (int i = 0; i < s.length(); i++) {
@@ -9,8 +13,8 @@ public class Q1347 {
         }
         int res = 0;
         for (int i : count) {
-            res += Math.abs(i);
+            res += Math.abs(i); // 绝对值
         }
-        return res / 2;
+        return res / 2; // 同上，结果除以2
     }
 }
--- leetcode/src/company/bloomberg/goodluck/Q1472.java
+++ leetcode/src/company/bloomberg/goodluck/Q1472.java
@@ -4,57 +4,71 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Stack;
 
+// 这题两种解法，看你觉得哪个更简单
 public class Q1472 {
+    // 第一种解法
     class BrowserHistory {
-        List<String> list = new ArrayList<>();
-        int index = 0, right = 0;
+        List<String> list = new ArrayList<>(); // 保存浏览记录
+        int index = 0, right = 0; // index表示当前的页面，right表示最前面的页面index
 
         public BrowserHistory(String homepage) {
             list.add(homepage);
         }
 
         public void visit(String url) {
+            // 如果現在在最前面的页面，加上这个url即可
             if (index == list.size() - 1) {
                 list.add(url);
                 index++;
+            // 否则，需要把index的下一个页面设置成这个url
             } else {
                 index++;
                 list.set(index, url);
             }
+            // 更新right为当前index，因为visit之后，这个url就是最新的页面了
             right = index;
         }
 
         public String back(int steps) {
+            // 从当前页面后退steps个页面得到index
             index = Math.max(0, index - steps);
             return list.get(index);
         }
 
         public String forward(int steps) {
+            // 从当前页面向前steps个页面得到index
             index = Math.min(right, index + steps);
             return list.get(index);
         }
     }
 
+    // 第二种解法
+    // 两个stack，在后退的过程中，把历史页面弹出加入未来页面
+    // 在前进的过程中，把未来的页面弹出加入历史页面
+    // 保持历史页面的栈顶始终是当前页面
     class BrowserHistory1 {
-        Stack<String> history = new Stack<>();
-        Stack<String> forward = new Stack<>();
+        Stack<String> history = new Stack<>(); // 表示历史页面
+        Stack<String> forward = new Stack<>(); // 表示向前的页面
 
         public BrowserHistory1(String homepage) {
             history.push(homepage);
         }
-
+        
+        // 在历史页面中添加url，把向前页面清空
         public void visit(String url) {
             history.push(url);
             forward.clear();
         }
-
+        
+        // 在后退的过程中，把history中的页面弹出并加入到向前的页面中
         public String back(int steps) {
             while (history.size() > 1 && steps-- > 0) {
                 forward.push(history.pop());
             }
             return history.peek();
         }
-
+        
+        // 在前进的过程中，把未来的页面弹出加入历史页面
         public String forward(int steps) {
             while (!forward.isEmpty() && steps-- > 0) {
                 history.push(forward.pop());
