46a3ab26f44a8d74df62d32083f119111aca6077
SawyerRen <51401596+SawyerRen@users.noreply.github.com>
2024-09-22 22:51:57 -0700

    Update Q1209.java

--- leetcode/src/company/bloomberg/goodluck/Q1209.java
+++ leetcode/src/company/bloomberg/goodluck/Q1209.java
@@ -4,8 +4,8 @@ import java.util.LinkedList;
 
 public class Q1209 {
     class Node {
-        char ch;
-        int count;
+        char ch; // char
+        int count; // 出现的次数
 
         public Node(char ch, int count) {
             this.ch = ch;
@@ -15,19 +15,22 @@ public class Q1209 {
 
     public String removeDuplicates(String s, int k) {
         LinkedList<Node> stack = new LinkedList<>();
+        // 遍历s
         for (char c : s.toCharArray()) {
+            // 如果stack不为空，并且当前字符和之前的字符相同，之前的字符出现的次数++，如果出现的次数到达k，从stack中去掉
             if (!stack.isEmpty() && c == stack.peekLast().ch) {
                 stack.peekLast().count++;
                 if (stack.peekLast().count == k) {
                     stack.removeLast();
                 }
+            // 否则，加入新的Node，char，出现的次数是1
             } else {
                 stack.addLast(new Node(c, 1));
             }
         }
         StringBuilder builder = new StringBuilder();
         while (!stack.isEmpty()) {
-            Node node = stack.removeFirst();
+            Node node = stack.removeFirst(); // 注意要按顺序，所以先removefirst
             for (int i = 0; i < node.count; i++) {
                 builder.append(node.ch);
             }
@@ -35,13 +38,19 @@ public class Q1209 {
         return builder.toString();
     }
 
+    // BBG一般会问下面这个问题，1D的candy crush，这题跟上面那题的区别是：
+    // 上面那道题一遇到k个重复的就删除，比如k = 3， s = "aaaa", 最后会剩下"a"，因为前三个a被移除了
+    // 这道题则是移除所有重复的，比如k = 3, s = "aaaa", 最后返回 "",空字符串
+    // 需要做的改变是，在遇到和之前不同的字符的时候，看前一个字符是不是要移除
     public String candyCrash(String s) {
         LinkedList<Node> stack = new LinkedList<>();
         for (char c : s.toCharArray()) {
             if (!stack.isEmpty()) {
+                // 如果遇到了新的字符，并且前面的字符出现次数>=3，移除前面的字符
                 if (stack.peekLast().ch != c && stack.peekLast().count >= 3) {
                     stack.removeLast();
                 }
+                // 否则，要么count++，要么新建一个Node，和上面一样
                 if (!stack.isEmpty() && stack.peekLast().ch == c) {
                     stack.peekLast().count++;
                 } else {
@@ -51,6 +60,7 @@ public class Q1209 {
                 stack.addLast(new Node(c, 1));
             }
         }
+        // 最后需要判断一下最后的Node，因为我们每次是去检查前一个，这样我们在循环中检查不了最后一个，需要在这边检查
         if (!stack.isEmpty() && stack.peekLast().count >= 3) {
             stack.removeLast();
         }
