5522c2fd0d4b4ae6c2213bc0bb91bc3fe691bbe5
SawyerRen <51401596+SawyerRen@users.noreply.github.com>
2024-09-22 17:27:06 -0700

    Update Q138.java

--- leetcode/src/company/bloomberg/goodluck/Q138.java
+++ leetcode/src/company/bloomberg/goodluck/Q138.java
@@ -18,12 +18,15 @@ public class Q138 {
 
     public Node copyRandomList(Node head) {
         if (head == null) return null;
+        // map保存原节点和复制的节点
         Map<Node, Node> map = new HashMap<>();
+        // 第一次遍历保存节点和对应的复制节点
         Node cur = head;
         while (cur != null) {
             map.put(cur, new Node(cur.val));
             cur = cur.next;
         }
+        // 第二次遍历设置next和random节点
         cur = head;
         while (cur != null) {
             map.get(cur).next = map.get(cur.next);
