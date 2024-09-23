bdd410bb2a87d02474730ba6de1d5b5b8d42a669
SawyerRen <51401596+SawyerRen@users.noreply.github.com>
2024-09-22 18:24:49 -0700

    Update Q1396.java

--- leetcode/src/company/bloomberg/goodluck/Q1396.java
+++ leetcode/src/company/bloomberg/goodluck/Q1396.java
@@ -7,7 +7,9 @@ import java.util.Map;
 
 public class Q1396 {
     class UndergroundSystem {
-        Map<Integer, Pair<String, Integer>> checkInMap = new HashMap<>();
+        // 保存一个id checkin的数据，<id, Pair<checkin的站名, checkin的时间>>
+        Map<Integer, Pair<String, Integer>> checkInMap = new HashMap<>(); 
+        // 保存一条路径耗时的信息, <路径, Pair<行驶的次数，行驶的总时间>>
         Map<String, Pair<Integer, Integer>> timeMap = new HashMap<>();
 
         public UndergroundSystem() {
@@ -20,18 +22,18 @@ public class Q1396 {
 
         // O(1)
         public void checkOut(int id, String stationName, int t) {
-            Pair<String, Integer> checkIn = checkInMap.get(id);
-            String route = checkIn.getKey() + "-" + stationName;
-            int duration = t - checkIn.getValue();
-            Pair<Integer, Integer> pair = timeMap.getOrDefault(route, new Pair<>(0, 0));
-            timeMap.put(route, new Pair<>(pair.getKey() + 1, pair.getValue() + duration));
+            Pair<String, Integer> checkIn = checkInMap.get(id); // 根据id拿到checkin的车站和时间
+            String route = checkIn.getKey() + "-" + stationName; // 用checkin车站-checkout车站组成String表示路径
+            int duration = t - checkIn.getValue(); // 行驶时长 = checkout时间 - checkin时间
+            Pair<Integer, Integer> pair = timeMap.getOrDefault(route, new Pair<>(0, 0)); // 取出路径的耗时信息
+            timeMap.put(route, new Pair<>(pair.getKey() + 1, pair.getValue() + duration)); // 行驶次数+1，行驶总时长+duration
         }
 
         // O(1)
         public double getAverageTime(String startStation, String endStation) {
-            String route = startStation + "-" + endStation;
-            Pair<Integer, Integer> pair = timeMap.get(route);
-            return (double) pair.getValue() / pair.getKey();
+            String route = startStation + "-" + endStation; // 车站组成路径
+            Pair<Integer, Integer> pair = timeMap.get(route); // 路径的耗时信息
+            return (double) pair.getValue() / pair.getKey(); // 平均时长 = 总时长/次数
         }
     }
 }
