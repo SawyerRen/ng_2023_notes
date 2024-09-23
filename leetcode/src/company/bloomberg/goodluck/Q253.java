bb5bbbe3d45375fdbeb3867b6cd634d06fe57ac8
SawyerRen <51401596+SawyerRen@users.noreply.github.com>
2024-09-22 17:44:23 -0700

    Update Q253.java

--- leetcode/src/company/bloomberg/goodluck/Q253.java
+++ leetcode/src/company/bloomberg/goodluck/Q253.java
@@ -6,16 +6,23 @@ import java.util.PriorityQueue;
 
 public class Q253 {
     public int minMeetingRooms(int[][] intervals) {
-        Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
-        int res = 0;
-        for (int[] interval : intervals) {
-            while (!pq.isEmpty() && pq.peek() <= interval[0]) {
-                pq.poll();
+        //Sort time
+        Arrays.sort(intervals, (a,b) -> a[0] - b[0]);
+
+        //minHeap
+        // int room = 0;
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+        minHeap.add(intervals[0][1]);//要先加一个进去
+        for(int i = 1; i < intervals.length; i++){  //这里的index 就从 1开始啦
+                 
+            if(intervals[i][0] >= minHeap.peek()){//只记录end 的时间就可以判断有没有结束了
+                //有结束的就poll出来
+                minHeap.poll();                
             }
-            pq.add(interval[1]);
-            res = Math.max(res, pq.size());
+            minHeap.add(intervals[i][1]);
+            // room = Math.max(room, minHeap.size());这里是替换不会减少房间
         }
-        return res;
+
+        return minHeap.size();//最后输出的是没办法替换的房间数量就是同时存在的房间数量
     }
 }
