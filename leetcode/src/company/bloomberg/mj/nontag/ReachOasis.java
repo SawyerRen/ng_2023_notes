70240c661ab7545a059103d561cf1e2ebb2594bd
SawyerRen <51401596+SawyerRen@users.noreply.github.com>
2024-09-23 23:01:29 -0700

    Update ReachOasis.java

--- leetcode/src/company/bloomberg/mj/nontag/ReachOasis.java
+++ leetcode/src/company/bloomberg/mj/nontag/ReachOasis.java
@@ -8,9 +8,17 @@ import java.util.Queue;
 import java.util.Set;
 
 /**
-有一个matrix，想象这个matrix是一个沙漠，中间有一个坐标是绿洲，有几个坐标是加油站
+这题有三步
+第一步
+有一个matrix，想象这个matrix是一个沙漠，中间有一个坐标是绿洲，你的车里有n个油，问你能不能到达绿洲
+直接判断距离和n的大小即可
+第二步
+matrix中间有几个坐标是加油站
 你从起点出发，每走一个要一个油，中间可以加一次油，问能不能到达绿洲
 起点用'c'表示，绿洲用'o'表示，加油站用数字表示，数字是加油站有的油的数量，比如'1'表示可以加一个油
+参考solution
+第三步
+matrix中有障碍物（用'z'表示），问你能不能走到，需要用BFS，参考solution2，这个有点复杂，最好能口述，面试官不要求写出来
 */
 public class ReachOasis {
 
@@ -49,6 +57,7 @@ public class ReachOasis {
     boolean solution2(char[][] matrix, int gas) {
         int[] start = null;
         int[] end = null;
+        // 先找出起点和绿洲
         int m = matrix.length, n = matrix[0].length;
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
@@ -61,22 +70,24 @@ public class ReachOasis {
         }
         Queue<int[]> queue = new LinkedList<>();
         Set<String> visited = new HashSet<>();
+        // queue中保存的是<x坐标, y坐标，剩余的油量，还可以加几次油>
         queue.add(new int[]{start[0], start[1], gas, 1});
+        // vsisited中保存的是(坐标+油量+可以加油的次数)
         visited.add(start[0] + "," + start[1] + "," + gas + ",1");
         int[][] dirs = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
         while (!queue.isEmpty()) {
             int[] poll = queue.poll();
             int i = poll[0], j = poll[1], curGas = poll[2], refill = poll[3];
-            if (i == end[0] && j == end[1]) return true;
-            if (curGas == 0) continue;
+            if (i == end[0] && j == end[1]) return true; // 到达绿洲
+            if (curGas == 0) continue; // 如果没有油了 直接continue
             for (int[] dir : dirs) {
                 int x = i + dir[0], y = j + dir[1];
-                if (x < 0 || x >= m || y < 0 || y >= n) continue;
-                int nextGas = curGas - 1;
-                if (Character.isDigit(matrix[x][y]) && refill == 1) {
+                if (x < 0 || x >= m || y < 0 || y >= n || matrix[x][y] == 'z') continue; // 走出matrix或者遇到障碍
+                int nextGas = curGas - 1; // 油量-1
+                if (Character.isDigit(matrix[x][y]) && refill == 1) { // 如果遇到加油站，并且还没有加过油，加油
                     nextGas += matrix[x][y] - '0';
                 }
-                if (visited.contains(x + "," + y + "," + nextGas + ",0")) continue;
+                if (visited.contains(x + "," + y + "," + nextGas + ",0")) continue; // 如果访问过，则continue
                 queue.add(new int[]{x, y, nextGas, 0});
             }
         }
